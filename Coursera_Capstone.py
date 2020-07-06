2bfa5cd65e675c4fde6233505e60b857a3115206
Srilatha V <srilathavemury20@gmail.com>
2020-07-06 18:19:50 -0400

    This is my first capstone project

---
+++ Coursera_Capstone.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# 1. Write some markdown to explain that this notebook will be mainly used for the capstone project.
+
+
+# "This is created for Data Science capstone project. I am excited to complete this course work"
+# 
+
+# In[2]:
+
+# 2. Import the pandas library as pd
+import pandas as pd
+
+
+# In[3]:
+
+# 3. Import the Numpy library as np
+import numpy as np
+
+
+# In[4]:
+
+# 4. Print the following the statement: Hello Capstone Project Course!
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
